b7feaf852d144d4a745de9c36d516e2675c97620
史 翔新 <shi_xiangxin@mages.co.jp>
2016-12-08 17:05:27 +0900

    Initial LICENSE, README and Xcode project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shiina
+This is a framework that contains some convenient methods and extensions for SpriteKit.
---
+++ Shiina/Shiina/Shiina.h
@@ -0,0 +1,19 @@
+//
+//  Shiina.h
+//  Shiina
+//
+//  Created by 史　翔新 on 2016/12/08.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shiina.
+FOUNDATION_EXPORT double ShiinaVersionNumber;
+
+//! Project version string for Shiina.
+FOUNDATION_EXPORT const unsigned char ShiinaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shiina/PublicHeader.h>
+
+
